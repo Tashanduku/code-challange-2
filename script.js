c7444ff1a58fea17176a0f53928c4a2e450f0349
Natasha Nduku <natashanduku0@gmail.com>
2024-12-10 16:48:06 +0300

    initializing arrays

---
+++ script.js
@@ -0,0 +1,8 @@
+// get shopping list from a local storage
+let todo = JSON.parse(localStorage.getItem("shoppinglist")) || [];
+const listInput = document.getElementById("listInput"); //fetching the input field
+const shopList = document.getElementById("shop-List"); //list section
+const addButton = document.querySelector(".btn"); // add button
+const deleteButton = document.getElementById("deleteButton"); //delete button
+const todoCount = document.getElementById("todoCount"); //counter section
+
